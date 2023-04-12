2518f4fad5fcde11368da6c2a21d2ee7526b34e6
Nikita Turda <nikitosturda5@gmail.com>
2023-04-12 22:49:08 +0300

    Created all the db tables and added them to the admin panel

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Thread(models.Model):
+    participants = models.ManyToManyField(User, limit_choices_to={'thread__lt': 2})
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+
+class Message(models.Model):
+    # saving all the messages from users that do not exist
+    sender = models.ForeignKey(User, on_delete=models.PROTECT)
+    text = models.TextField(max_length=400)
+    thread = models.ForeignKey(Thread, on_delete=models.PROTECT)  # saving thread with user that do not exist
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+
